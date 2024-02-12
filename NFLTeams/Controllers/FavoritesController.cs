d012961c6c42c8456a6a65c9cfb8b07f161502bc
Nicholas Woerner <nicholaswoerner@gmail.com>
2024-02-11 19:45:27 -0600

    Add project files.

---
+++ NFLTeams/Controllers/FavoritesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamListViewModel
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams()
+            };
+
+            ViewBag.HttpContext = HttpContext;
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete()
+        {
+            var session = new NFLSession(HttpContext.Session);
+            var cookies = new NFLCookies(HttpContext.Response.Cookies);
+
+            session.RemoveMyTeams();
+            cookies.RemoveMyTeamIds();
+
+            TempData["message"] = "Favorite teams cleared";
+
+            return RedirectToAction("Index", "Home",
+                new {
+                    ActiveConf = session.GetActiveConf(),
+                    ActiveDiv = session.GetActiveDiv()
+                });
+        }
+    
+    }
+}
---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Change(TeamListViewModel model)
+        {
+            if (model != null)
+            {
+                string userName = model.UserName;
+
+                var session = new NFLSession(HttpContext.Session);
+                session.SetUserName(userName);
+
+                string activeConf = session.GetActiveConf();
+                string activeDiv = session.GetActiveDiv();
+
+                return RedirectToAction("Index", "Home", new { ActiveConf = activeConf, ActiveDiv = activeDiv});
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
